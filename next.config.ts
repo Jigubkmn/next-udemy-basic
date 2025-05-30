e043262f45077c6f667e037c9bbd95dae1889b3e
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-30 20:19:19 +0900

    画像の外部サービスを事前に許可する

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 画像のURLを事前に許可する
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.dog.ceo",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
