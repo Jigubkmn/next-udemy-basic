e90a8bec8926535196f264f484b9607bb5ba102c
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-31 18:27:04 +0900

    フォーム送信処理

---
+++ src/lib/actions/contact.ts
@@ -0,0 +1,14 @@
+"use server"
+import { redirect } from "next/navigation"
+
+// formData：フォームデータを取得
+export async function submitContactForm(formData: FormData) {
+  const name = formData.get("name")
+  const email = formData.get("email")
+  console.log(name, email)
+
+  // バリデーション
+  //　DB登録
+
+  redirect("/contacts/complete")
+}
